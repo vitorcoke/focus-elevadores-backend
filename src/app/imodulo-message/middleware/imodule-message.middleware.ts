1ef3fa8b32e23841efa02112b820ffb7233bfaf7
Vitor Coke <cokevitor@gmail.com>
2022-11-01 10:25:32 -0300

    teste

--- src/app/imodulo-message/middleware/imodule-message.middleware.ts
+++ src/app/imodulo-message/middleware/imodule-message.middleware.ts
@@ -9,7 +9,7 @@ export class IModuleMessageMiddleware implements NestMiddleware {
 
     const validateHash = JSON.stringify(body).length * 34712;
 
-    console.log(body, headers);
+    console.log(body, headers, validateHash);
 
     if (
       headers.authorization === `Bearer ${process.env.IMODULE_TOKEN}` &&
